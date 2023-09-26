2e22f318c3e6dde7db4c8907f25cd84a8dd76087
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-09-26 23:55:46 +0545

    feat: gold log

--- gahana.py
+++ gahana.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 URL='https://gahanaonline.com/gold-rate-history/'
 page = requests.get(URL);
@@ -9,16 +10,13 @@ soup = BeautifulSoup(page.content, 'html.parser');
 table = soup.find('table')
 
 elements = table.find_all("tr")
-
+ 
 results = []
 for element in elements:
-    children = element.findChildren()
-    for child in children:
-        data = {}
-        print(child)
-        # data.date_ad = child[0].text
-        # data.date_bs = child[1].text
-        # data.amount = child[2].text
-        # results.insert(data)
+    children = element.findChildren("td")
+    results.append({'date_ad': children[0].text.strip(),'date_bs': children[1].text.strip(),'amount': children[2].text.strip().replace("/-","")})
 
-print(results)    
+
+with open('goldlog.json', 'w', encoding='latin-1') as f:
+    json.dump(results, f, indent=8, ensure_ascii=False)
+print("Created Json File")
